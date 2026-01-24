50bc4b6db464c3e1594fc836cff879dd8dcbdc7d
Radu Gherman <gherman.raduioan@gmail.com>
2026-01-24 10:35:25 +0200

    initial commit

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("hello world")
+# cross platform - can use on any pltf - linux unix 
+# interpreted language --> folosesc interpretorii --> pot fi folositi usor in alt os
+# alea compilate trb rezolvate pt os-ul folosit curent (C++)
+# pypi - loc unde sunt puse poachete dezvoltate de comunitate
