82737d9bf5ffc1d72355e749a98395211fe40943
Radu Gherman <gherman.raduioan@gmail.com>
2026-01-28 19:29:25 +0200

    day 6- almost 1 week

---
+++ day6-28.02.26.py
@@ -0,0 +1,8 @@
+# spent most of my time on a test
+# git init
+# git add .
+# git commit -m "mesaj"
+# git branch -M master
+# git push -u origin master
+# with open ("day 1 notes- 23.01.26.txt") as f:
+#     print( f.readlines())
