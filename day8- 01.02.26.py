4d25fcbdc9482db0ef7df6353583e4ae4d6f42ba
Radu Gherman <gherman.raduioan@gmail.com>
2026-02-01 19:29:23 +0200

    day 8- just a test for csv

---
+++ day8- 01.02.26.py
@@ -0,0 +1,8 @@
+# spent most of my time on a test
+# git init
+# git add .
+# git commit -m "mesaj"
+# git branch -M master
+# git push -u origin master
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
