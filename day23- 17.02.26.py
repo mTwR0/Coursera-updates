008673cc12f5a1c7069b9b517fc55dcbbac4682f
Radu Gherman <gherman.raduioan@gmail.com>
2026-02-17 19:26:34 +0200

    data streams, exit codes

---
+++ day23- 17.02.26.py
@@ -0,0 +1,14 @@
+# linux shell -> 
+# $PATH --> calea unde cauta executabilele - le cauta in ordine de la prima cale pana la ultima
+# import sys
+# print(sys.argv) --> sunt argumentele pe care le dai scriptului cand rulezi --> ./test.py arg1 arg2
+# 
+# value of the status of the last exec command = ? --> daca vrei s avezi ce e nauntru --> $?
+# wc = count nr of lines, words, chr in a file 
+# sys.exit(23123) in python ca sa ii spui sa iasa cu un cod de eroare specific
+#
+#   env variables
+#   import os --> os.environ.get(cheie) | os.environ.get(cheie, val_daca_nu_gaseste)
+#   
+#
+#
